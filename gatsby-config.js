35a12057ec4badca8116b5422837b7a3d4e15377
Fahad Almusharraf <ifahad7@gmail.com>
2017-11-25 01:22:29 -0500

    Fix syntax highlighting

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,14 @@ module.exports = {
     {
       resolve: "gatsby-transformer-remark",
       options: {
-        plugins: []
-      }
-    },
-    {
-      resolve: "gatsby-remark-prismjs",
-      options: {
-        classPrefix: "language-"
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-"
+            }
+          }
+        ]
       }
     }
   ]
